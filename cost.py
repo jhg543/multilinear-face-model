7a6412ba4f07bcbb90667f7b58a93cd57ae5a2dc
z089 <z089@msn.com>
2018-04-04 17:40:04 +0800

    optimize of single frame landmark

--- cost.py
+++ cost.py
@@ -1,22 +1,18 @@
 import numpy as np
 import vertex_screen as vt
 
-dim_id = 40
-dim_exp = 46
 
-
-def f_id_f(x, gt_landmarks, n_frames, f_blend_shape, f_):
-    u = x[0:dim_id]
-    f = x[dim_id]
+# layout:  u (n_dim) f (1)  n_frames * [r (3) t(3) e (n_exp-1)]
+def f_id_f(x, m_vertex, gt_landmarks, n_frames, n_id, f_blend_shape, w_reg):
+    u = x[0:n_id]
+    f = x[n_id]
     # p =  [  r (3)  t(3) e (dim_exp)   , n_frames  ]
-    p = x[dim_id + 1:].reshape((n_frames, -1))
-    r = p[:, 0:3]
-    t = p[:, 3:6]
+    p = x[n_id + 1:].reshape((n_frames, -1))
     e = p[:, 6:]
-    e = np.c_[e, 1 - np.sum(e, axis=1)]
+    e = np.c_[1 - np.sum(e, axis=1), e]
     blend_shape = f_blend_shape(u)  # (dim_exp, n_v_flat)
     vertexes = e @ blend_shape  # (n_frame,n_v_flat)
-
+    vertexes = vertexes + m_vertex
     # add w=1
     vertexes = vertexes.reshape((n_frames, -1, 3))
     vertexes = np.concatenate([vertexes, np.ones((vertexes.shape[0], vertexes.shape[1], 1))], axis=2)
@@ -25,11 +21,152 @@ def f_id_f(x, gt_landmarks, n_frames, f_blend_shape, f_):
     m_r1 = vt.build_rotate_matrix(p[:, 0], 0, 1)
     m_r2 = vt.build_rotate_matrix(p[:, 1], 0, 2)
     m_r3 = vt.build_rotate_matrix(p[:, 2], 1, 2)
-    m_t = vt.build_transition_matrix(p[3:6])
+    m_t = vt.build_transition_matrix(p[:, 3:6])
 
     m_proj = vt.build_projection_matrix(f)
 
     mat = m_proj @ (m_t @ m_r1 @ m_r2 @ m_r3)
-    vertexes = vertexes @ mat
-    vertexes = vertexes[:, 0:2] / vertexes[:, 2][:, None]
-    vertexes -= gt_landmarks
+    vertexes = np.transpose(vertexes, (0, 2, 1))
+    vertexes = mat @ vertexes
+    vertexes = np.transpose(vertexes, (0, 2, 1))
+    vertexes = vertexes[:, :, 0:2] / vertexes[:, :, 3][:, :, None]
+    e_landmarks_1 = vertexes - gt_landmarks
+
+    e_reg_1 = np.average(p, axis=0) - p
+    return np.concatenate([e_landmarks_1.flatten(), e_reg_1.flatten() * w_reg, np.array((np.sum(u ** 2) - 1,))])
+
+
+def build_initial_guess_and_bound(n_frames, u, n_exp):
+    n_exp_1 = n_exp - 1
+    f_guess = np.array([1.5], dtype=np.float32)
+    rt_guess = np.array([0, 0, 0, 0, 0, -2], dtype=np.float32)
+    e_guess = np.ones(n_exp_1, dtype=np.float32) / n_exp_1
+    p_row = np.concatenate((rt_guess, e_guess))
+    initial_guess = np.concatenate((u, f_guess, np.tile(p_row, n_frames)))
+    p_lbound = np.concatenate(
+        (np.array([-2, -2, -2, -np.inf, -np.inf, -np.inf], dtype=np.float32),
+         np.full(e_guess.shape, 0, dtype=np.float32)))
+    p_ubound = np.concatenate(
+        (np.array([2, 2, 2, np.inf, np.inf, 0], dtype=np.float32), np.full(e_guess.shape, 1, dtype=np.float32)))
+    lbound = np.concatenate((
+        np.full(u.shape, -1, dtype=np.float32),
+        np.full((1,), 0, dtype=np.float32),
+        np.tile(p_lbound, n_frames)
+    ))
+    ubound = np.concatenate((
+        np.full(u.shape, 1, dtype=np.float32),
+        np.full((1,), np.inf, dtype=np.float32),
+        np.tile(p_ubound, n_frames)
+    ))
+    return initial_guess, (lbound, ubound)
+
+
+# x0 = 1  # initial guess
+# optimize_result = op.least_squares(f_id_f, x0, verbose=2, args=(gt_landmarks, n_frames, f_blend_shape, w_reg))
+
+
+def build_initial_guess_and_bound_p(n_frames, u, n_exp):
+    n_exp_1 = n_exp - 1
+    f_guess = np.array([1.5], dtype=np.float32)
+    rt_guess = np.array([0, 0, 0, 0, 0, -2], dtype=np.float32)
+    e_guess = np.ones(n_exp_1, dtype=np.float32) / n_exp_1
+    p_row = np.concatenate((rt_guess, e_guess))
+    initial_guess = np.concatenate((u, f_guess, np.tile(p_row, n_frames)))
+    p_lbound = np.concatenate(
+        (np.full(rt_guess.shape, -np.inf, dtype=np.float32), np.full(e_guess.shape, 0, dtype=np.float32)))
+    p_ubound = np.concatenate(
+        (np.full(rt_guess.shape, np.inf, dtype=np.float32), np.full(e_guess.shape, 1, dtype=np.float32)))
+    lbound = np.concatenate((
+        np.full(u.shape, -1, dtype=np.float32),
+        np.full((1,), 0, dtype=np.float32),
+        np.tile(p_lbound, n_frames)
+    ))
+    ubound = np.concatenate((
+        np.full(u.shape, 1, dtype=np.float32),
+        np.full((1,), np.inf, dtype=np.float32),
+        np.tile(p_ubound, n_frames)
+    ))
+    return initial_guess, (lbound, ubound)
+
+
+def f_p(x, m_vertex, gt_landmarks, f, blend_shape, w_reg):
+    e = x[6:]
+    e = np.concatenate([np.array(1 - np.sum(e))[None], e])
+    vertexes = e @ blend_shape  # (n_frame,n_v_flat)
+    vertexes = vertexes + m_vertex
+    # add w=1
+    # vertexes = vt.rt_multiframe(vertexes,x[None,0:6],f)
+    vertexes = vertexes.reshape((-1, 3))
+    vertexes = np.concatenate([vertexes, np.ones((vertexes.shape[0], 1))], axis=1)
+
+    # build transform matrix
+    m_r1 = vt.build_rotate_matrix(x[None, 0], 0, 1)
+    m_r2 = vt.build_rotate_matrix(x[None, 1], 0, 2)
+    m_r3 = vt.build_rotate_matrix(x[None, 2], 1, 2)
+    m_t = vt.build_transition_matrix(x[None, 3:6])
+
+    m_proj = vt.build_projection_matrix(f)
+
+    mat = m_proj @ (m_t @ m_r1 @ m_r2 @ m_r3)[0]
+    vertexes = np.transpose(vertexes)
+    vertexes = mat @ vertexes
+    vertexes = np.transpose(vertexes)
+    vertexes = vertexes[:, 0:2] / vertexes[:, 3][:, None]
+
+    e_landmarks_1 = vertexes - gt_landmarks
+    return np.concatenate([e_landmarks_1.flatten(), e * w_reg])
+
+
+def f_p_2(x, m_vertex, gt_landmarks, f, blend_shape, w_reg):
+    e = x[6:]
+    e = np.concatenate([np.array(1 - np.sum(e))[None], e])
+    vertexes = e @ blend_shape  # (n_frame,n_v_flat)
+    vertexes = vertexes + m_vertex
+    # add w=1
+    vertexes = vt.rt_multiframe(vertexes, x[None, 0:6], f)
+
+    e_landmarks_1 = vertexes - gt_landmarks
+    return np.concatenate([e_landmarks_1.flatten(), e * w_reg])
+
+
+def f_p_f_2(x, m_vertex, gt_landmarks, blend_shape, w_reg):
+    e = x[7:]
+    f = x[6]
+    e = np.concatenate([np.array(1 - np.sum(e))[None], e])
+    vertexes = e @ blend_shape  # (n_frame,n_v_flat)
+    vertexes = vertexes + m_vertex
+    # add w=1
+    vertexes = vt.rt_multiframe(vertexes, x[None, 0:6], f)
+
+    e_landmarks_1 = vertexes - gt_landmarks
+    return np.concatenate([e_landmarks_1.flatten(), e * w_reg])
+
+
+def f_sq(x, fp, *args):
+    return np.sum(fp(x, *args) ** 2)
+
+
+def f_rt(x, m_vertex, gt_landmarks, f, blend_shape, w_reg):
+    vertexes = blend_shape[7]  # (n_frame,n_v_flat)
+    vertexes = vertexes + m_vertex
+    # add w=1
+    vertexes = vertexes.reshape((-1, 3))
+    vertexes = np.concatenate([vertexes, np.ones((vertexes.shape[0], 1))], axis=1)
+
+    # build transform matrix
+    m_r1 = vt.build_rotate_matrix(x[None, 0], 0, 1)
+    m_r2 = vt.build_rotate_matrix(x[None, 1], 0, 2)
+    m_r3 = vt.build_rotate_matrix(x[None, 2], 1, 2)
+    m_t = vt.build_transition_matrix(x[None, 3:6])
+
+    m_proj = vt.build_projection_matrix(f)
+
+    mat = m_proj @ (m_t @ m_r1 @ m_r2 @ m_r3)[0]
+    vertexes = np.transpose(vertexes)
+    vertexes = mat @ vertexes
+    vertexes = np.transpose(vertexes)
+    vertexes = vertexes[:, 0:2] / vertexes[:, 3][:, None]
+    if vertexes.shape != gt_landmarks.shape:
+        raise ValueError("shape mismatch")
+    e_landmarks_1 = vertexes - gt_landmarks
+    return np.concatenate([e_landmarks_1.flatten()])
--- svddraw.py
+++ svddraw.py
@@ -5,7 +5,9 @@ import numpy as np
 import draw
 import fwmesh
 import landmark_detector
-import vertex_screen
+import vertex_screen as vt
+import cost
+import scipy.optimize as op
 
 
 def get_blend_shape(core_id, v_id):
@@ -39,9 +41,42 @@ def truncate_core_tensor(core_tensor, mean_vertex_axis, landmarks):
     return core_tensor[:, :, index], mean_vertex_axis[index]
 
 
+def build_test_gt_landmark(vv, t3r3, focus_length):
+    a = np.array([vt.proj(vt.rt(vec_to_xyzw(vv), *(t3r3[i])), focus_length).transpose()
+                  for i in range(t3r3.shape[0])])
+    return a[:, :, 0:2] / a[:, :, 3][:, :, None]
+
+
+def do_bootstrap(d, md, ui, gt_landmarks):
+    test_initial_guess = np.concatenate([[0, 0, 0, 0.001, 0.001, -2, 1], np.zeros(46)])
+    bound_bfgs = [(-2, 2)] * 3 + [(None, None)] * 2 + [(None, -1)] + [(0, None)] + [(0, 1)] * 46
+    test_bs = get_blend_shape(d, np.average(ui, axis=0))
+    guess_frame0, e_frame0, info = op.fmin_l_bfgs_b(cost.f_sq, test_initial_guess, bounds=bound_bfgs, approx_grad=True,
+                                                    args=(cost.f_p_f_2, md, gt_landmarks[0], test_bs, 0.01))
+    print("e_frame0={} f={}".format(e_frame0, guess_frame0[6]))
+    guess_frames = [guess_frame0]
+    for i in range(1, gt_landmarks.shape[0]):
+        # ig = np.average(guess_frames, axis=0)
+        gf, ef, ei = op.fmin_l_bfgs_b(cost.f_sq, test_initial_guess, bounds=bound_bfgs,
+                                      approx_grad=True,
+                                      args=(cost.f_p_f_2, md, gt_landmarks[i], test_bs, 0.01))
+        print("e_frame{}={} f={} r={} t={}".format(i, ef, gf[6], gf[0:3], gf[3:6]))
+        guess_frames.append(gf)
+
+    return guess_frames[0]
+
+
+def build_landmark_img(dx, w):
+    hw = w // 2
+    img = np.zeros((w, w, 3), np.uint8)
+    for i in range(dx.shape[0]):
+        cv2.circle(img, (int(-dx[i, 0] * hw + hw), int(-dx[i, 1] * hw + hw)), 2, (255, 0, 255))
+    return img
+
+
 def test():
-    focus_length = 1.5
-    min_keyframes = 20
+    focus_length = 0.8
+    min_keyframes = 3
     keyframes = []
     vertex, triangle, landmark = fwmesh.read_mesh_def()
     draw.write_parameters(0, vertex)
@@ -50,53 +85,115 @@ def test():
     draw.write_parameters(3, focus_length)
     draw.write_parameters(5, 0.7)
     draw.write_parameters(6, 1.33)
-    draw.start_render_loop_thread()
+
     se, ue, si, ui, c, m = fwmodel.load_compact_svd('C:\\dev\\3dface\\svd2', 40, 47)
     c = T.mode_dot(c, ue, 1)  # we don't need SVD on exp axis
 
     d, md = truncate_core_tensor(c, m, landmark)
-    rx = vertex_screen.rt(vec_to_xyzw(get_blend_shape(d, ui[8])[7] + md), 0, 0, -2, 0.1, 0.1,
-                          0.1).transpose()
-    dx = vertex_screen.proj(rx.transpose(), focus_length).transpose()
-    print(rx[0])
+    vv = get_blend_shape(d, ui[8])[7] + md
+    test_gt_id8_exp7 = build_test_gt_landmark(vv, np.array([[1, 1, -2, 0.1, 0.1, 0.1],
+                                                            [-0.5, 1, -2, -0.1, 0.1, 0.1],
+                                                            [1, 0, -4, 0.1, 0.2, -0.1],
+                                                            [1, 0, -3, 0.1, 0.1, 0.1],
+                                                            [1, 0, -3, 0.1, 0.1, 0.1]
+                                                            ]), focus_length)
+    dx = test_gt_id8_exp7[0]
+    print(dx[0])
 
-    rv = vertex_screen.rt(vec_to_xyzw(get_blend_shape(c, ui[8])[7] + m), 0, 0, -2, 0.1, 0.1,
-                          0.1).transpose()
+    def fb(v_id):
+        return get_blend_shape(d, v_id)
+
+    cam_landmarks = np.fromfile("C:\\dev\\3dface\\keyframes\\landmarks", dtype=np.float32, count=min_keyframes * 68 * 2) \
+        .reshape(
+        (min_keyframes, 68, 2))
+
+    # initial_guess, bounds = cost.build_initial_guess_and_bound(min_keyframes, ui[0], 47)
+    # print(initial_guess)
+
+    # gt_landmarks = test_gt_id8_exp7
+    # r = op.least_squares(cost.f_id_f, initial_guess, bounds=bounds, verbose=2, x_scale='jac',
+    #                      args=(md, gt_landmarks, min_keyframes, ui[0].shape[0], fb, 0.001))
+    # print(r.x)
+    # print(r.fun)
+
+    # test_bs = get_blend_shape(d, ui[8])
+    # gt_landmarks = dx
+    # print(gt_landmarks.shape)
+    # initial_e_guess = np.zeros(46)
+    # initial_e_guess[6] = 0
+
+    # test_initial_guess = np.concatenate([[1, 1, 1, 0.001, 0.001, -3], initial_e_guess])
+    # test_bound_lower = np.concatenate([[-2, -2, -2, -np.inf, -np.inf, -np.inf], np.zeros(46)])
+    # test_bound_upper = np.concatenate([[2, 2, 2, np.inf, np.inf, 0], np.ones(46)])
+    #
+    # r = op.least_squares(cost.f_p, test_initial_guess, verbose=2, method="trf",
+    #                      bounds=(test_bound_lower, test_bound_upper),
+    #                      args=(md, gt_landmarks, focus_length, test_bs, 0.001))
+    #
+    # print(r.x)
+    # print(r.grad)
+    # print(r.jac)
+
+
+
+    guess_frame0 = do_bootstrap(d, md, ui, cam_landmarks * (-1))
+    guess_r3 = guess_frame0[0:3]
+    guess_t3 = guess_frame0[3:6]
+    guess_f = guess_frame0[6]
+    guess_e = guess_frame0[7:]
+
+    guess_e = np.concatenate([np.array(1 - np.sum(guess_e))[None], guess_e])
+    rv = vt.rt(vec_to_xyzw(guess_e @ get_blend_shape(c, np.average(ui, axis=0)) + m), *guess_t3, *guess_r3).transpose()
     draw.write_parameters(0, rv)
-    print(rv[landmark[0]])
-    scx = vertex_screen.proj(rv.transpose(), focus_length).transpose()
-    print(scx[landmark[0]])
-    cap = cv2.VideoCapture(1)
-    ret, frame = cap.read()
-    if frame is None:
-        raise Exception('摄像头坏了。')
-    h, w, c = frame.shape
-    draw.write_parameters(6, w / h)
-    print(w, h)
-    long_edge = w
-    if h > w:
-        long_edge = h
-    while True:
-        ret, frame = cap.read()
-        if frame is None:
-            raise Exception('摄像头坏了。')
-        img = frame.copy()
-        d = landmark_detector.detect_landmark_68(img)
-        if d is not None:
-            if len(keyframes) < min_keyframes:
-                keyframes.append(landmark_detection_to_screen_xy(d, w, h))
-            for i in range(68):
-                x, y = d[i]
-                cv2.circle(img, (x, y), 3, (0, 255, 255))
-                cv2.putText(img, "{}".format(i), (x, y), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255))
-        # for i in landmark:
-        #     sc = screen_xyzw_to_pixel(scx[i], long_edge // 2, w // 2, h // 2)
-        #     cv2.circle(img, sc, 2, (255, 0, 255))
-        for i in range(dx.shape[0]):
-            sc = screen_xyzw_to_pixel(dx[i], long_edge // 2, w // 2, h // 2)
-            cv2.circle(img, sc, 2, (255, 0, 255))
-            cv2.putText(img, "{}".format(i), sc, cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255))
-        draw.write_parameters(4, img)
+    draw.write_parameters(3, guess_f)
+
+    dx = cam_landmarks[0] * -1
+    landmark_image = build_landmark_img(dx, 600)
+    draw.write_parameters(4, landmark_image)
+    draw.write_parameters(6, 1)
+
+    draw.start_render_window_thread(1200)
+
+
+    # cap = cv2.VideoCapture(1)
+    # ret, frame = cap.read()
+    # if frame is None:
+    #     raise Exception('摄像头坏了。')
+    # h, w, c = frame.shape
+    # draw.write_parameters(6, w / h)
+    # print(w, h)
+    # long_edge = w
+    # if h > w:
+    #     long_edge = h
+    # while True:
+    #     ret, frame = cap.read()
+    #     if frame is None:
+    #         raise Exception('摄像头坏了。')
+    #     img = frame.copy()
+    #     d = landmark_detector.detect_landmark_68(img)
+    #     if d is not None:
+    #         for i in range(68):
+    #             x, y = d[i]
+    #             cv2.circle(img, (x, y), 3, (0, 255, 255))
+    #             cv2.putText(img, "{}".format(i), (x, y), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255))
+    #         if len(keyframes) < min_keyframes:
+    #             keyframes.append(landmark_detection_to_screen_xy(d, w, h))
+    #             idx = len(keyframes)
+    #             cv2.imwrite("C:\\dev\\3dface\\keyframes\\{}.jpg".format(idx), img)
+    #             print(idx)
+    #             if idx == min_keyframes:
+    #                 a = np.array(keyframes)
+    #                 a.tofile("C:\\dev\\3dface\\keyframes\\landmarks")
+    #                 print(a.dtype, a.shape)
+    #
+    #     # for i in landmark:
+    #     #     sc = screen_xyzw_to_pixel(scx[i], long_edge // 2, w // 2, h // 2)
+    #     #     cv2.circle(img, sc, 2, (255, 0, 255))
+    #     # for i in range(dx.shape[0]):
+    #     #     sc = screen_xyzw_to_pixel(dx[i], long_edge // 2, w // 2, h // 2)
+    #     #     cv2.circle(img, sc, 2, (255, 0, 255))
+    #     #     cv2.putText(img, "{}".format(i), sc, cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255))
+    #     draw.write_parameters(4, img)
 
 
 if __name__ == '__main__':
